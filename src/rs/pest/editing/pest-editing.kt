f80305ce26760bdd97410dc7923e9bf02633a0d0
ice1000 <ice1000kotlin@foxmail.com>
2019-03-03 20:03:21 -0500

    [ parser ] Add parser definition

--- src/rs/pest/editing/pest-editing.kt
+++ src/rs/pest/editing/pest-editing.kt
@@ -1,8 +1,13 @@
 package rs.pest.editing
 
+import com.intellij.lang.BracePair
 import com.intellij.lang.Commenter
+import com.intellij.lang.PairedBraceMatcher
+import com.intellij.psi.PsiFile
+import com.intellij.psi.tree.IElementType
 import rs.pest.PEST_BLOCK_COMMENT_BEGIN
 import rs.pest.PEST_BLOCK_COMMENT_END
+import rs.pest.psi.PestTypes
 
 class PestCommenter : Commenter {
 	override fun getCommentedBlockCommentPrefix() = blockCommentPrefix
@@ -10,4 +15,17 @@ class PestCommenter : Commenter {
 	override fun getBlockCommentPrefix() = PEST_BLOCK_COMMENT_BEGIN
 	override fun getBlockCommentSuffix() = PEST_BLOCK_COMMENT_END
 	override fun getLineCommentPrefix() = "// "
+}
+
+class PestBraceMatcher : PairedBraceMatcher {
+	private companion object Pairs {
+		private val PAIRS = arrayOf(
+			BracePair(PestTypes.OPENING_BRACK, PestTypes.CLOSING_BRACK, false),
+			BracePair(PestTypes.OPENING_PAREN, PestTypes.CLOSING_PAREN, false),
+			BracePair(PestTypes.OPENING_BRACE, PestTypes.CLOSING_BRACE, false))
+	}
+
+	override fun getCodeConstructStart(file: PsiFile?, openingBraceOffset: Int) = openingBraceOffset
+	override fun isPairedBracesAllowedBeforeType(lbraceType: IElementType, contextType: IElementType?) = true
+	override fun getPairs() = PAIRS
 }
--- src/rs/pest/psi/pest-tokens.kt
+++ src/rs/pest/psi/pest-tokens.kt
@@ -4,16 +4,39 @@ import com.intellij.lang.ASTNode
 import com.intellij.lang.ParserDefinition
 import com.intellij.lexer.FlexAdapter
 import com.intellij.openapi.project.Project
+import com.intellij.psi.FileViewProvider
 import com.intellij.psi.PsiElement
+import com.intellij.psi.stubs.PsiFileStubImpl
 import com.intellij.psi.tree.IElementType
+import com.intellij.psi.tree.IStubFileElementType
 import com.intellij.psi.tree.TokenSet
+import rs.pest.PestFile
 import rs.pest.PestLanguage
 import rs.pest.psi.PestTypes.*
 
 class PestTokenType(debugName: String) : IElementType(debugName, PestLanguage.INSTANCE) {
 	companion object Static {
-		@JvmField val PEST_COMMENT = PestTokenType("comment")
-		@JvmField val COMMENTS = TokenSet.create(PEST_COMMENT)
-		@JvmField val STRINGS = TokenSet.create(PEST_STRING, PEST_STRING_TOKEN, PEST_CHARACTER, PEST_CHAR_TOKEN)
+		@JvmField
+		val PEST_COMMENT = PestTokenType("comment")
+		@JvmField
+		val COMMENTS = TokenSet.create(PEST_COMMENT)
+		@JvmField
+		val STRINGS = TokenSet.create(STRING, STRING_TOKEN, CHARACTER, CHAR_TOKEN)
 	}
+}
+
+class PestParserDefinition : ParserDefinition {
+	private companion object {
+		private val FILE = IStubFileElementType<PsiFileStubImpl<PestFile>>(PestLanguage.INSTANCE)
+	}
+
+	override fun createParser(project: Project?) = PestParser()
+	override fun createLexer(project: Project?) = FlexAdapter(PestLexer())
+	override fun createElement(node: ASTNode?): PsiElement = PestTypes.Factory.createElement(node)
+	override fun createFile(viewProvider: FileViewProvider) = PestFile(viewProvider)
+	override fun getStringLiteralElements() = PestTokenType.STRINGS
+	override fun getCommentTokens() = PestTokenType.COMMENTS
+	override fun getFileNodeType() = FILE
+	// TODO: replace after dropping support for 183
+	override fun spaceExistanceTypeBetweenTokens(left: ASTNode?, right: ASTNode?) = ParserDefinition.SpaceRequirements.MAY
 }
