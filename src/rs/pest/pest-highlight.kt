84f97561864e46e3327320ffab995dc317d2520f
ice1000 <ice1000kotlin@foxmail.com>
2019-03-03 20:43:58 -0500

    [ highlight ] More

--- src/rs/pest/pest-highlight.kt
+++ src/rs/pest/pest-highlight.kt
@@ -1,7 +1,6 @@
 package rs.pest
 
 import com.intellij.openapi.editor.DefaultLanguageHighlighterColors
-import com.intellij.openapi.editor.HighlighterColors
 import com.intellij.openapi.editor.colors.TextAttributesKey
 import com.intellij.openapi.fileTypes.SyntaxHighlighter
 import com.intellij.openapi.fileTypes.SyntaxHighlighterFactory
@@ -19,32 +18,36 @@ import rs.pest.psi.PestTypes
 
 object PestHighlighter : SyntaxHighlighter {
 	@JvmField val KEYWORD = TextAttributesKey.createTextAttributesKey("PEST_KEYWORD", DefaultLanguageHighlighterColors.KEYWORD)
-	@JvmField val RULE = TextAttributesKey.createTextAttributesKey("PEST_BUILTIN_NAME", DefaultLanguageHighlighterColors.FUNCTION_CALL)
-	@JvmField val SYMBOL = TextAttributesKey.createTextAttributesKey("PEST_SYMBOL", HighlighterColors.TEXT)
+	@JvmField val IDENTIFIER = TextAttributesKey.createTextAttributesKey("PEST_IDENTIFIER", DefaultLanguageHighlighterColors.IDENTIFIER)
 	@JvmField val NUMBER = TextAttributesKey.createTextAttributesKey("PEST_NUMBER", DefaultLanguageHighlighterColors.NUMBER)
 	@JvmField val STRING = TextAttributesKey.createTextAttributesKey("PEST_STRING", DefaultLanguageHighlighterColors.STRING)
 	@JvmField val CHAR = TextAttributesKey.createTextAttributesKey("PEST_CHAR", DefaultLanguageHighlighterColors.STRING)
 	@JvmField val OPERATOR = TextAttributesKey.createTextAttributesKey("PEST_OPERATOR", DefaultLanguageHighlighterColors.OPERATION_SIGN)
-	@JvmField val BRACKET = TextAttributesKey.createTextAttributesKey("PEST_PARENTHESES", DefaultLanguageHighlighterColors.PARENTHESES)
-	@JvmField val B_BRACKET = TextAttributesKey.createTextAttributesKey("PEST_BRACES", DefaultLanguageHighlighterColors.BRACES)
-	@JvmField val M_BRACKET = TextAttributesKey.createTextAttributesKey("PEST_BRACKET", DefaultLanguageHighlighterColors.BRACKETS)
-	@JvmField val COMMENT = TextAttributesKey.createTextAttributesKey("LINE_COMMENT", DefaultLanguageHighlighterColors.LINE_COMMENT)
+	@JvmField val PAREN = TextAttributesKey.createTextAttributesKey("PEST_PARENTHESES", DefaultLanguageHighlighterColors.PARENTHESES)
+	@JvmField val BRACE = TextAttributesKey.createTextAttributesKey("PEST_BRACES", DefaultLanguageHighlighterColors.BRACES)
+	@JvmField val BRACK = TextAttributesKey.createTextAttributesKey("PEST_BRACKET", DefaultLanguageHighlighterColors.BRACKETS)
+	@JvmField val COMMENT = TextAttributesKey.createTextAttributesKey("PEST_LINE_COMMENT", DefaultLanguageHighlighterColors.LINE_COMMENT)
 	@JvmField val BLOCK_COMMENT = TextAttributesKey.createTextAttributesKey("PEST_BLOCK_COMMENT", DefaultLanguageHighlighterColors.BLOCK_COMMENT)
 
 	@JvmField val KEYWORD_KEY = arrayOf(KEYWORD)
+	@JvmField val IDENTIFIER_KEY = arrayOf(IDENTIFIER)
+	@JvmField val OPERATOR_KEY = arrayOf(OPERATOR)
 	@JvmField val STRING_KEY = arrayOf(STRING)
 	@JvmField val CHAR_KEY = arrayOf(CHAR)
 	@JvmField val NUMBER_KEY = arrayOf(NUMBER)
-	@JvmField val BRACKETS_KEY = arrayOf(BRACKET)
-	@JvmField val B_BRACKETS_KEY = arrayOf(B_BRACKET)
-	@JvmField val M_BRACKETS_KEY = arrayOf(M_BRACKET)
+	@JvmField val PAREN_KEY = arrayOf(PAREN)
+	@JvmField val BRACE_KEY = arrayOf(BRACE)
+	@JvmField val BRACK_KEY = arrayOf(BRACK)
 	@JvmField val COMMENT_KEY = arrayOf(COMMENT)
 	@JvmField val BLOCK_COMMENT_KEY = arrayOf(BLOCK_COMMENT)
 
 	private val KEYWORDS_LIST = listOf(
 		PestTypes.PUSH_TOKEN,
-		PestTypes.PEEK_TOKEN
-	)
+		PestTypes.PEEK_TOKEN)
+
+	private val OPERATORS_LIST = listOf(
+		PestTypes.PUSH_TOKEN,
+		PestTypes.PEEK_TOKEN)
 
 	/** brackets */
 	private val BRACKS = listOf(PestTypes.OPENING_BRACK, PestTypes.CLOSING_BRACK)
@@ -55,16 +58,18 @@ object PestHighlighter : SyntaxHighlighter {
 
 	override fun getHighlightingLexer() = lexer()
 	override fun getTokenHighlights(type: IElementType?): Array<TextAttributesKey> = when (type) {
+		PestTypes.IDENTIFIER_TOKEN -> IDENTIFIER_KEY
 		PestTypes.STRING_TOKEN -> STRING_KEY
 		PestTypes.CHAR_TOKEN -> CHAR_KEY
 		PestTokenType.LINE_COMMENT -> COMMENT_KEY
 		PestTokenType.BLOCK_COMMENT -> BLOCK_COMMENT_KEY
 		PestTypes.NUMBER,
 		PestTypes.MINUS -> NUMBER_KEY
-		in BRACKS -> BRACKETS_KEY
-		in PARENS -> M_BRACKETS_KEY
-		in BRACES -> B_BRACKETS_KEY
+		in PARENS-> PAREN_KEY
+		in BRACKS -> BRACK_KEY
+		in BRACES -> BRACE_KEY
 		in KEYWORDS_LIST -> KEYWORD_KEY
+		in OPERATORS_LIST -> OPERATOR_KEY
 		else -> emptyArray()
 	}
 }
@@ -76,6 +81,10 @@ class PestHighlighterFactory : SyntaxHighlighterFactory() {
 class PestColorSettingsPage : ColorSettingsPage {
 	private companion object DescriptorHolder {
 		private val DESCRIPTORS = arrayOf(
+			AttributesDescriptor(PestBundle.message("pest.highlighter.settings.identifier"), PestHighlighter.IDENTIFIER),
+			AttributesDescriptor(PestBundle.message("pest.highlighter.settings.number"), PestHighlighter.NUMBER),
+			AttributesDescriptor(PestBundle.message("pest.highlighter.settings.string"), PestHighlighter.STRING),
+			AttributesDescriptor(PestBundle.message("pest.highlighter.settings.char"), PestHighlighter.CHAR),
 			AttributesDescriptor(PestBundle.message("pest.highlighter.settings.comment"), PestHighlighter.COMMENT),
 			AttributesDescriptor(PestBundle.message("pest.highlighter.settings.block-comment"), PestHighlighter.BLOCK_COMMENT))
 
