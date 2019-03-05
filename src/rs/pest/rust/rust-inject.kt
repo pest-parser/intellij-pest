a2796f902a9c6e32e1bd372943b08b7c09ef126c
ice1000 <ice1000kotlin@foxmail.com>
2019-03-04 21:06:07 -0500

    [ rust ] Inject inline grammar

--- src/rs/pest/rust/rust-inject.kt
+++ src/rs/pest/rust/rust-inject.kt
@@ -5,20 +5,22 @@ import com.intellij.psi.InjectedLanguagePlaces
 import com.intellij.psi.LanguageInjector
 import com.intellij.psi.PsiLanguageInjectionHost
 import com.intellij.psi.PsiWhiteSpace
-import org.rust.lang.core.psi.ext.RsLitExprMixin
-import org.rust.lang.core.psi.ext.RsMetaItemImplMixin
+import org.rust.lang.core.psi.RsLitExpr
+import org.rust.lang.core.psi.RsMetaItem
+import org.rust.lang.core.psi.RsOuterAttr
 import org.rust.lang.core.psi.ext.childrenWithLeaves
 import rs.pest.PestLanguage
 
 class InlineGrammarInjector : LanguageInjector {
 	override fun getLanguagesToInject(host: PsiLanguageInjectionHost, places: InjectedLanguagePlaces) {
-		if (host !is RsLitExprMixin) return
+		if (host !is RsLitExpr) return
 		if (isInlineGrammar(host)) places.addPlace(PestLanguage.INSTANCE, TextRange(1, host.textLength - 1), null, null)
 	}
 
-	/// Do we need to check the `#[derive(Parser)]` as well?
-	private fun isInlineGrammar(host: RsLitExprMixin): Boolean {
-		val parent = host.parent as? RsMetaItemImplMixin ?: return false
+	/// Do we need to check the existence of `#[derive(Parser)]` as well?
+	private fun isInlineGrammar(host: RsLitExpr): Boolean {
+		val parent = host.parent as? RsMetaItem ?: return false
+		if (parent.parent !is RsOuterAttr) return false
 		val peers = parent.childrenWithLeaves.filter { it !is PsiWhiteSpace }.toList()
 		if (peers.size != 3) return false
 		if (peers[0].text != "grammar_inline") return false
