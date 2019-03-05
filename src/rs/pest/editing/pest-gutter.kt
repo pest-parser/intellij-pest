bc6aa3bc7264ed905f75af3b2039f44954f00144
ice1000 <ice1000kotlin@foxmail.com>
2019-03-04 22:39:11 -0500

    [ line marker ] Implement recursion line marker

---
+++ src/rs/pest/editing/pest-gutter.kt
@@ -0,0 +1,41 @@
+package rs.pest.editing
+
+import com.intellij.codeInsight.daemon.LineMarkerInfo
+import com.intellij.codeInsight.daemon.LineMarkerProvider
+import com.intellij.icons.AllIcons
+import com.intellij.openapi.actionSystem.AnAction
+import com.intellij.openapi.editor.markup.GutterIconRenderer
+import com.intellij.openapi.progress.ProgressManager
+import com.intellij.psi.PsiElement
+import com.intellij.psi.SyntaxTraverser
+import com.intellij.util.FunctionUtil
+import rs.pest.psi.PestTypes
+import rs.pest.psi.impl.PestGrammarRuleMixin
+
+class PestRecursionLineMarkerProvider : LineMarkerProvider {
+	override fun getLineMarkerInfo(element: PsiElement): LineMarkerInfo<*>? = null
+	override fun collectSlowLineMarkers(elements: List<PsiElement>, result: MutableCollection<LineMarkerInfo<*>>) {
+		elements
+			.asSequence()
+			.filterIsInstance<PestGrammarRuleMixin>()
+			.filter { root ->
+				ProgressManager.checkCanceled()
+				val grammarBody = root.grammarBody ?: return@filter false
+				val name = root.name ?: return@filter false
+				SyntaxTraverser
+					.psiTraverser(grammarBody)
+					.filterTypes { it == PestTypes.IDENTIFIER }
+					.any { it.text == name }
+			}
+			.map(::RecMarkerInfo)
+			.forEach { result.add(it) }
+	}
+
+	private class RecMarkerInfo internal constructor(id: PsiElement) : LineMarkerInfo<PsiElement>(
+		id, id.textRange, AllIcons.Gutter.RecursiveMethod, FunctionUtil.constant("Recursive rule"), null, GutterIconRenderer.Alignment.RIGHT) {
+		override fun createGutterRenderer(): LineMarkerGutterIconRenderer<PsiElement>? =
+			if (myIcon == null) null else object : LineMarkerGutterIconRenderer<PsiElement>(this) {
+				override fun getClickAction(): AnAction? = null
+			}
+	}
+}
