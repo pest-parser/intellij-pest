24b820ffbef416e65e2b34a186ed1fb4fa59c88b
ice1000 <ice1000kotlin@foxmail.com>
2019-04-16 14:31:57 -0400

    [ live preview ] Virtual Psi

--- src/rs/pest/action/tools.kt
+++ src/rs/pest/action/tools.kt
@@ -3,11 +3,29 @@ package rs.pest.action
 import com.intellij.ide.browsers.BrowserLauncher
 import com.intellij.openapi.actionSystem.AnAction
 import com.intellij.openapi.actionSystem.AnActionEvent
+import com.intellij.openapi.actionSystem.CommonDataKeys
 import com.intellij.openapi.project.DumbAware
+import com.intellij.psi.util.PsiTreeUtil
 import rs.pest.PEST_WEBSITE
+import rs.pest.PestFile
+import rs.pest.livePreview.livePreview
 
 class PestViewSiteAction : AnAction(), DumbAware {
 	override fun actionPerformed(e: AnActionEvent) {
 		BrowserLauncher.instance.open(PEST_WEBSITE)
+	}
+}
+
+class PestLivePreviewAction : AnAction(), DumbAware {
+	override fun update(e: AnActionEvent) {
+		super.update(e)
+		val psiFile = CommonDataKeys.PSI_FILE.getData(e.dataContext)
+		e.presentation.isEnabledAndVisible = psiFile is PestFile && !PsiTreeUtil.hasErrorElements(psiFile)
+	}
+
+	override fun actionPerformed(e: AnActionEvent) {
+		val psiFile = CommonDataKeys.PSI_FILE.getData(e.dataContext)
+			as? PestFile ?: return
+		livePreview(psiFile)
 	}
 }
--- src/rs/pest/livePreview/live-preview.kt
+++ src/rs/pest/livePreview/live-preview.kt
@@ -1,14 +1,41 @@
 package rs.pest.livePreview
 
+import com.intellij.extapi.psi.ASTWrapperPsiElement
 import com.intellij.extapi.psi.PsiFileBase
+import com.intellij.lang.ASTNode
+import com.intellij.lang.LightPsiParser
+import com.intellij.lang.PsiBuilder
+import com.intellij.lang.PsiParser
+import com.intellij.openapi.fileEditor.ex.FileEditorManagerEx
 import com.intellij.openapi.fileTypes.LanguageFileType
 import com.intellij.openapi.fileTypes.PlainTextParserDefinition
+import com.intellij.openapi.project.Project
 import com.intellij.psi.FileViewProvider
+import com.intellij.psi.PsiElement
+import com.intellij.psi.PsiElementVisitor
+import com.intellij.psi.impl.PsiManagerEx
+import com.intellij.psi.tree.IElementType
 import com.intellij.psi.tree.IFileElementType
+import com.intellij.testFramework.LightVirtualFile
 import icons.PestIcons
+import rs.pest.PestFile
+import javax.swing.SwingConstants
+
+fun livePreview(file: PestFile) {
+	val project = file.project
+	val virtualFile = LightVirtualFile("${file.name}.preview", LivePreviewFileType, "")
+	val psiFile = PsiManagerEx.getInstanceEx(project).findFile(virtualFile) ?: return
+//	PsiDocumentManager.getInstance(project).getDocument(file)?.addDocumentListener(object : DocumentListener {
+//		override fun documentChanged(event: DocumentEvent) {
+//		}
+//	})
+	val editorManager = FileEditorManagerEx.getInstanceEx(project)
+	editorManager.currentWindow.split(SwingConstants.HORIZONTAL, false, virtualFile, true)
+	editorManager.openFile(virtualFile, true)
+}
 
 object LivePreviewFileType : LanguageFileType(LivePreviewLanguage.INSTANCE) {
-	override fun getDefaultExtension() = "PEST_EXTENSION"
+	override fun getDefaultExtension() = "preview"
 	override fun getName() = LivePreviewLanguage.INSTANCE.displayName
 	override fun getIcon() = PestIcons.PEST_FILE
 	override fun getDescription() = LivePreviewLanguage.INSTANCE.displayName
@@ -18,9 +45,35 @@ class LivePreviewFile(viewProvider: FileViewProvider) : PsiFileBase(viewProvider
 	override fun getFileType() = LivePreviewFileType
 }
 
+class LivePreviewParser : PsiParser, LightPsiParser {
+	override fun parseLight(root: IElementType?, builder: PsiBuilder?) {
+		parse(root ?: return, builder ?: return)
+	}
+	override fun parse(root: IElementType, builder: PsiBuilder): ASTNode {
+		val mark = builder.mark()
+		run {
+			@Suppress("NAME_SHADOWING")
+			val mark = builder.mark()
+			builder.advanceLexer()
+			mark.done(LivePreviewElement.TYPE)
+		}
+		mark.done(root)
+		return builder.treeBuilt
+	}
+}
+
+class LivePreviewElement(node: ASTNode) : ASTWrapperPsiElement(node) {
+	companion object Tokens {
+		@JvmField val TYPE = IElementType("LivePreview Content", LivePreviewLanguage.INSTANCE)
+	}
+}
+
 class LivePreviewParserDefinition : PlainTextParserDefinition() {
 	override fun getFileNodeType() = FILE
 	override fun createFile(viewProvider: FileViewProvider) = LivePreviewFile(viewProvider)
+	override fun createParser(project: Project?) = LivePreviewParser()
+	override fun createElement(node: ASTNode) = LivePreviewElement(node)
+
 	companion object {
 		@JvmField val FILE = IFileElementType(LivePreviewLanguage.INSTANCE)
 	}
