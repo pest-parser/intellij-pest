4177a034cb62172d3d51e84e1395b8ce5a503c25
ice1000 <ice1000kotlin@foxmail.com>
2019-04-17 20:54:37 -0400

    [ live preview ] This error handling is not very satisfying

--- src/rs/pest/livePreview/live-highlight.kt
+++ src/rs/pest/livePreview/live-highlight.kt
@@ -2,8 +2,9 @@ package rs.pest.livePreview
 
 import com.intellij.lang.annotation.AnnotationHolder
 import com.intellij.lang.annotation.Annotator
+import com.intellij.openapi.application.ApplicationManager
+import com.intellij.openapi.editor.EditorFactory
 import com.intellij.openapi.editor.markup.TextAttributes
-import com.intellij.openapi.fileEditor.ex.FileEditorManagerEx
 import com.intellij.openapi.ui.popup.Balloon
 import com.intellij.openapi.ui.popup.JBPopupFactory
 import com.intellij.openapi.util.TextRange
@@ -12,6 +13,7 @@ import com.intellij.psi.PsiDocumentManager
 import com.intellij.psi.PsiElement
 import com.intellij.ui.JBColor
 import com.intellij.ui.components.JBTextArea
+import com.intellij.xml.util.XmlStringUtil
 import org.intellij.lang.annotations.Language
 import rs.pest.PestBundle
 import java.awt.Color
@@ -34,14 +36,14 @@ fun textAttrFromDoc(docComment: PsiComment) =
 private val lexicalRegex = Regex("\\A(\\d+)\\^(\\d+)\\^(.*)$")
 
 @Language("RegExp")
-private val errMsgRegex = Regex("\\A\\s+-->\\s+(\\d+):(\\d+)$")
+private val errMsgRegex = Regex("\\A\\s+-->\\s+(\\d+):(\\d+)\\p{all}*")
 
 class LivePreviewAnnotator : Annotator {
 	override fun annotate(element: PsiElement, holder: AnnotationHolder) {
 		if (element !is LivePreviewFile) return
 		val pestFile = element.pestFile ?: return
 		val ruleName = element.ruleName ?: return
-		val rules = pestFile.rules().mapNotNull { it.docComment?.let { doc -> it.name to doc } }.toMap()
+		val rules = pestFile.rules().map { it.name to it }.toMap()
 		if (rules.isEmpty()) return
 		if (pestFile.errors.any()) return
 		if (pestFile.availableRules.none()) return
@@ -64,27 +66,34 @@ class LivePreviewAnnotator : Annotator {
 				val offset = lineStart + col - 1
 				val range = if (offset >= dom.textLength) TextRange(length - 1, length)
 				else TextRange(offset, offset + 1)
-				holder.createErrorAnnotation(range, res.msg)
-			} ?: run {
+				holder.createErrorAnnotation(range, null).apply {
+					//language=HTML
+					tooltip = XmlStringUtil.wrapInHtml(res.msg).replace("\n", "<br/>")
+				}
+			} ?: ApplicationManager.getApplication().invokeLater {
+				val project = element.project
+				val dom = PsiDocumentManager.getInstance(project).getDocument(element) ?: return@invokeLater
 				val panel = JPanel().apply { add(JBTextArea().apply { text = res.msg }) }
-				val editor = FileEditorManagerEx.getInstanceEx(element.project)
-					?.getEditors(element.virtualFile)
-					?.firstOrNull()
-					?: return@run
+				val editor = EditorFactory.getInstance()
+					.getEditors(dom, project)
+					.firstOrNull()
+					?: return@invokeLater
 				val factory = JBPopupFactory.getInstance()
 				factory
 					.createBalloonBuilder(panel)
 					.setTitle(PestBundle.message("pest.annotator.live-preview.error.title"))
 					.setFillColor(JBColor.RED)
 					.createBalloon()
-					.show(factory.guessBestPopupLocation(editor.component), Balloon.Position.below)
+					.show(factory.guessBestPopupLocation(editor), Balloon.Position.below)
 			}
-			is Rendering.Ok -> res.lexical.mapNotNull { lexicalRegex.matchEntire(it) }.forEach {
+			is Rendering.Ok -> res.lexical.mapNotNull(lexicalRegex::matchEntire).forEach {
 				val (_, start, end, rule) = it.groupValues
-				val docComment = rules[rule] ?: return@forEach
-				val attr = textAttrFromDoc(docComment) ?: return@forEach
+				val psiRule = rules[rule] ?: return@forEach
 				val range = TextRange(start.toInt(), end.toInt())
-				holder.createInfoAnnotation(range, rule).enforcedTextAttributes = attr
+				val annotation = holder.createInfoAnnotation(range, rule)
+				psiRule.docComment?.let(::textAttrFromDoc)?.let { attr ->
+					annotation.enforcedTextAttributes = attr
+				}
 			}
 		}
 	}
