26ada423d1cb1f2f11aa7fdbf1c886f7321b2d29
ice1000 <ice1000kotlin@foxmail.com>
2019-09-25 22:11:48 -0400

    [ html ] This should somehow work

--- src/rs/pest/action/tools.kt
+++ src/rs/pest/action/tools.kt
@@ -1,18 +1,28 @@
 package rs.pest.action
 
+import com.intellij.notification.Notification
+import com.intellij.notification.NotificationType
+import com.intellij.notification.Notifications
 import com.intellij.openapi.actionSystem.AnAction
 import com.intellij.openapi.actionSystem.AnActionEvent
 import com.intellij.openapi.actionSystem.CommonDataKeys
 import com.intellij.openapi.fileEditor.ex.FileEditorManagerEx
+import com.intellij.openapi.progress.PerformInBackgroundOption
+import com.intellij.openapi.progress.ProgressIndicator
+import com.intellij.openapi.progress.ProgressManager
+import com.intellij.openapi.progress.Task
 import com.intellij.openapi.project.DumbAware
 import com.intellij.openapi.ui.DialogBuilder
 import com.intellij.openapi.ui.popup.Balloon
 import com.intellij.openapi.ui.popup.JBPopupFactory
+import com.intellij.openapi.util.text.StringUtil
 import com.intellij.psi.util.PsiTreeUtil
 import rs.pest.PestBundle
 import rs.pest.PestFile
 import rs.pest.action.ui.PestIdeBridgeInfoImpl
 import rs.pest.action.ui.RuleSelector
+import rs.pest.livePreview.LivePreviewFile
+import rs.pest.livePreview.defaultPreviewToHtml
 import rs.pest.livePreview.livePreview
 
 class PestViewInfoAction : AnAction(), DumbAware {
@@ -22,6 +32,25 @@ class PestViewInfoAction : AnAction(), DumbAware {
 			.title(PestBundle.message("pest.actions.info.title"))
 			.centerPanel(component)
 			.show()
+	}
+}
+
+class PestLivePreviewToHtmlAction : AnAction() {
+	override fun update(e: AnActionEvent) {
+		super.update(e)
+		e.presentation.isEnabledAndVisible = CommonDataKeys.PSI_FILE.getData(e.dataContext) is LivePreviewFile
+	}
+
+	override fun actionPerformed(e: AnActionEvent) {
+		val file = CommonDataKeys.PSI_FILE.getData(e.dataContext) as? LivePreviewFile ?: return
+		ProgressManager.getInstance().run(object : Task.Backgroundable(file.project, "HTML Generation", true, PerformInBackgroundOption.ALWAYS_BACKGROUND) {
+			override fun run(indicator: ProgressIndicator) {
+				val startTime = System.currentTimeMillis()
+				defaultPreviewToHtml(file, indicator)
+				val s = "HTML file generated in ${StringUtil.formatDuration(System.currentTimeMillis() - startTime)}"
+				Notifications.Bus.notify(Notification("HTML Export", "", s, NotificationType.INFORMATION))
+			}
+		})
 	}
 }
 
--- src/rs/pest/livePreview/html.kt
+++ src/rs/pest/livePreview/html.kt
@@ -1,24 +1,53 @@
 package rs.pest.livePreview
 
+import com.intellij.openapi.progress.ProgressIndicator
+import com.intellij.openapi.vfs.VfsUtil
 import kotlinx.html.*
 import kotlinx.html.stream.appendHTML
 import java.awt.Color
+import java.io.File
 
-fun toHtml(file: LivePreviewFile, html: Appendable) {
+fun defaultPreviewToHtml(file: LivePreviewFile, indicator: ProgressIndicator) {
+	val fileName = "${file.pestFile?.name}-${file.ruleName}.html"
+	val ioFile = File(fileName)
+	ioFile.writer().use {
+		toHtml(file, it)
+		it.flush()
+	}
+	VfsUtil.findFileByIoFile(ioFile, true)
+}
+
+private fun toHtml(file: LivePreviewFile, html: Appendable) {
 	html.appendHTML().html {
-		head { title("${file.pestFile?.name}-${file.ruleName}") }
+		head { charset("UTF-8") }
 		comment("Generated with love by IntelliJ-Pest")
-		val highlights = arrayOfNulls<Pair<Color?, String>>(file.textLength)
+		val chars = file.textToCharArray()
+		val highlights = arrayOfNulls<Pair<Color?, String>>(chars.size)
 		highlight(file, { range, err ->
 			for (i in range.startOffset..range.endOffset) highlights[i] = null to err.orEmpty()
 		}) { range, info, attributes ->
 			for (i in range.startOffset..range.endOffset)
 				highlights[i] = attributes.foregroundColor to info
 		}
-		body {
-			pre {
-				TODO()
-			}
-		}
+		body { pre { render(highlights, chars) } }
 	}
+}
+
+private fun PRE.render(highlights: Array<Pair<Color?, String>?>, chars: CharArray) {
+	for ((i, highlight) in highlights.withIndex())
+		if (highlight == null) +chars[i].toString()
+		else {
+			val color = highlight.first
+			if (color == null) {
+				TODO("highlight parse failures")
+			} else font(color) { +chars[i].toString() }
+		}
+}
+
+private class FONT(consumer: TagConsumer<*>, color: Color) : HTMLTag("font",
+	consumer, mapOf("color" to "#${color.rgb}"), inlineTag = true, emptyTag = false
+), HtmlInlineTag
+
+private fun PRE.font(color: Color, block: FONT.() -> Unit = {}) {
+	FONT(consumer, color).visit(block)
 }
