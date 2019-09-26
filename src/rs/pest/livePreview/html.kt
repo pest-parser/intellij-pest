8f45aaa0de12bcdd6eb5ee45a01d2eb1bbd18bf8
ice1000 <ice1000kotlin@foxmail.com>
2019-09-25 22:17:55 -0400

    [ html ] Fix exception, some user experience work

--- src/rs/pest/livePreview/html.kt
+++ src/rs/pest/livePreview/html.kt
@@ -1,6 +1,9 @@
 package rs.pest.livePreview
 
+import com.intellij.openapi.application.ReadAction
+import com.intellij.openapi.progress.ProcessCanceledException
 import com.intellij.openapi.progress.ProgressIndicator
+import com.intellij.openapi.progress.ProgressIndicatorProvider
 import com.intellij.openapi.vfs.VfsUtil
 import kotlinx.html.*
 import kotlinx.html.stream.appendHTML
@@ -11,13 +14,15 @@ fun defaultPreviewToHtml(file: LivePreviewFile, indicator: ProgressIndicator) {
 	val fileName = "${file.pestFile?.name}-${file.ruleName}.html"
 	val ioFile = File(fileName)
 	ioFile.writer().use {
-		toHtml(file, it)
+		toHtml(file, it, indicator)
 		it.flush()
 	}
 	VfsUtil.findFileByIoFile(ioFile, true)
 }
 
-private fun toHtml(file: LivePreviewFile, html: Appendable) {
+private fun toHtml(file: LivePreviewFile, html: Appendable, indicator: ProgressIndicator) {
+	indicator.text = "Initializing highlighting info"
+	indicator.fraction = 0.0
 	html.appendHTML().html {
 		head { charset("UTF-8") }
 		comment("Generated with love by IntelliJ-Pest")
@@ -29,12 +34,16 @@ private fun toHtml(file: LivePreviewFile, html: Appendable) {
 			for (i in range.startOffset..range.endOffset)
 				highlights[i] = attributes.foregroundColor to info
 		}
-		body { pre { render(highlights, chars) } }
+		indicator.text = "Writing html"
+		indicator.fraction = 0.2
+		body { pre { ReadAction.run<ProcessCanceledException> { render(highlights, chars, indicator) } } }
 	}
 }
 
-private fun PRE.render(highlights: Array<Pair<Color?, String>?>, chars: CharArray) {
-	for ((i, highlight) in highlights.withIndex())
+private fun PRE.render(highlights: Array<Pair<Color?, String>?>, chars: CharArray, indicator: ProgressIndicator) {
+	ProgressIndicatorProvider.checkCanceled()
+	for ((i, highlight) in highlights.withIndex()) {
+		indicator.fraction = 0.2 + (i.toDouble() / chars.size) * 0.8
 		if (highlight == null) +chars[i].toString()
 		else {
 			val color = highlight.first
@@ -42,6 +51,7 @@ private fun PRE.render(highlights: Array<Pair<Color?, String>?>, chars: CharArra
 				TODO("highlight parse failures")
 			} else font(color) { +chars[i].toString() }
 		}
+	}
 }
 
 private class FONT(consumer: TagConsumer<*>, color: Color) : HTMLTag("font",
